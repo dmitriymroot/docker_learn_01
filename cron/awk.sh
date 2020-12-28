3247836d63301c1552f5210f51607125e8654a64
DmitriyM <dmitriymroot@gmail.com>
2020-12-28 03:51:13 -0600

    cron v2

--- cron/awk.sh
+++ cron/awk.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-find /mnt/data/logs -type f -iname "*.log" -exec awk '/invalid/ {print $1,$2,$9,$11}' {} \; >> /mnt/data/out/outputlogs
-find /mnt/data/logs -type f -iname "*.log" -exec awk '!/invalid/ {print $1,$2,$7,$9}' {} \; >> /mnt/data/out/outputlogs
+
+find /mnt/data/logs -type f -iname "*.log" -exec awk '/Failed password for invalid/ {print $1,$2,$9,$11}' {} \; >> /mnt/data/out/outputlogs
+find /mnt/data/logs -type f -iname "*.log" -exec awk '/Invalid user/ {print $1,$2,$6,$8}' {} \; >> /mnt/data/out/outputlogs
+find /mnt/data/logs -type f -iname "*.log" -exec awk '!/[Ii]nvalid/ {print $1,$2,$7,$9}' {} \; >> /mnt/data/out/outputlogs
 sed -i -e 's/+03:00 / /g' -e 's/T/ /g' /mnt/data/out/outputlogs
-rm -rf /mnt/data/logs/*
+find /mnt/data/logs -type f -iname "*.log" -exec truncate -s 0 {} \;
+
+
 
